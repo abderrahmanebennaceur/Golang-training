601fa4c65fab6f466d52b4b20f83027edeeccc67
abderrahmanebennaceur <abderrahmane.bennaceur.213@gmail.com>
2023-06-15 08:45:25 +0100

    output example

---
+++ output.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello", "world")
+	fmt.Println("7+8", 7+8)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(1 > 2)
+}
